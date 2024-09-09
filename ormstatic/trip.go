1cbc815cbc44d563d7c483b9209fd7c3d345a69b
Atsu06 <f1912201426@gmail.com>
2024-09-09 18:54:39 +0900

    fix: geomカラムを含むテーブルを変更

--- ormstatic/trip.go
+++ ormstatic/trip.go
@@ -1,18 +1,21 @@
 package ormstatic
 
+import geomdatatypes "github.com/ITNS-LAB/gtfs-gorm/internal/gormdatatypes"
+
 type Trip struct {
 	RouteId              *string `gorm:"index;not null"`
 	ServiceId            *string `gorm:"index;not null"`
 	TripId               *string `gorm:"primaryKey"`
 	TripHeadsign         *string
 	TripShortName        *string
-	DirectionId          *int        `gorm:"index"`
-	BlockId              *string     `gorm:"index"`
-	ShapeId              *string     `gorm:"index"`
-	WheelchairAccessible *int        `gorm:"default:0"`
-	BikesAllowed         *int        `gorm:"default:0"`
-	StopTimes            []StopTime  `gorm:"foreignKey:TripId;references:TripId"`
-	Frequencies          []Frequency `gorm:"foreignKey:TripId;references:TripId"`
+	DirectionId          *int                    `gorm:"index"`
+	BlockId              *string                 `gorm:"index"`
+	ShapeId              *string                 `gorm:"index"`
+	WheelchairAccessible *int                    `gorm:"default:0"`
+	BikesAllowed         *int                    `gorm:"default:0"`
+	Geom                 *geomdatatypes.Geometry `gorm:"index"`
+	StopTimes            []StopTime              `gorm:"foreignKey:TripId;references:TripId"`
+	Frequencies          []Frequency             `gorm:"foreignKey:TripId;references:TripId"`
 }
 
 func (Trip) TableName() string {
