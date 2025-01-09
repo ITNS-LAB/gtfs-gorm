6e624f66276feda24d87af808ebaba01ffb66951
syota <syoutaintennis@gmail.com>
2025-01-09 15:31:12 +0900

    add:GTFS scheduleのmodel trips の追加

---
+++ gtfsschedule/trips.go
@@ -0,0 +1,14 @@
+package gtfsschedule
+
+type Trips struct {
+	RouteId              string `gorm:"index;not null"`
+	ServiceId            string `gorm:"index;not null"`
+	TripId               string `gorm:"primaryKey"`
+	TripHeadsign         *string
+	TripShortName        *string
+	DirectionId          *int    `gorm:"index"`
+	BlockId              *string `gorm:"index"`
+	ShapeId              *string `gorm:"index"`
+	WheelchairAccessible *int    `gorm:"default:0"`
+	BikesAllowed         *int    `gorm:"default:0"`
+}
